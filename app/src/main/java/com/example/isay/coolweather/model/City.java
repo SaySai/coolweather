37240c6da413eb92197a9a2fb33b0f2633640d66
Harry <whosay1992@gmail.com>
2016-01-30 20:39:16 +0800

    新增数据库帮助类，以以及各表对应的实体类。

---
+++ app/src/main/java/com/example/isay/coolweather/model/City.java
@@ -0,0 +1,46 @@
+package com.example.isay.coolweather.model;
+
+/**
+ * Created by isay on 1/30/2016.
+ */
+public class City {
+    private int id;
+
+    private String cityName;
+
+    private String cityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
